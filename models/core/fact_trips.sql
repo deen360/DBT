f9b85451524b54ff45f7745efe5187333498fa90
YUSUF KAMORUDEEN <kamaldeen32@gmail.com>
2022-06-28 11:28:40 +0000

    homework

--- models/core/fact_trips.sql
+++ models/core/fact_trips.sql
@@ -2,20 +2,11 @@
 
 with green_data as (
     select *, 
-        'Green' as service_type 
-    from {{ ref('stg_green_tripdata') }}
-), 
-
-yellow_data as (
-    select *, 
-        'Yellow' as service_type
-    from {{ ref('stg_yellow_tripdata') }}
+    from {{ ref('stg_fhv_tripdata_external_table') }}
 ), 
 
 trips_unioned as (
     select * from green_data
-    union all
-    select * from yellow_data
 ), 
 
 dim_zones as (
@@ -23,35 +14,12 @@ dim_zones as (
     where borough != 'Unknown'
 )
 select 
-    trips_unioned.tripid, 
-    trips_unioned.vendorid, 
-    trips_unioned.service_type,
-    trips_unioned.ratecodeid, 
+    trips_unioned.tripid,
+    trips_unioned.dispatching_base_num, 
     trips_unioned.pickup_locationid, 
-    pickup_zone.borough as pickup_borough, 
-    pickup_zone.zone as pickup_zone, 
-    trips_unioned.dropoff_locationid,
-    dropoff_zone.borough as dropoff_borough, 
-    dropoff_zone.zone as dropoff_zone,  
+    trips_unioned.dropoff_locationid,  
     trips_unioned.pickup_datetime, 
     trips_unioned.dropoff_datetime, 
-    trips_unioned.store_and_fwd_flag, 
-    trips_unioned.passenger_count, 
-    trips_unioned.trip_distance, 
-    trips_unioned.trip_type, 
-    trips_unioned.fare_amount, 
-    trips_unioned.extra, 
-    trips_unioned.mta_tax, 
-    trips_unioned.tip_amount, 
-    trips_unioned.tolls_amount, 
-    trips_unioned.ehail_fee, 
-    trips_unioned.improvement_surcharge, 
-    trips_unioned.total_amount, 
-    trips_unioned.payment_type, 
-    trips_unioned.payment_type_description, 
-    trips_unioned.congestion_surcharge
+    trips_unioned.Affiliated_base_number, 
+
 from trips_unioned
-inner join dim_zones as pickup_zone
-on trips_unioned.pickup_locationid = pickup_zone.locationid
-inner join dim_zones as dropoff_zone
-on trips_unioned.dropoff_locationid = dropoff_zone.locationid
---
+++ models/staging/stg_fhv_tripdata_external_table.sql
@@ -0,0 +1,35 @@
+{{ config(materialized='view') }}
+
+
+with tripdata as 
+(
+  select dispatching_base_num,pickup_datetime,dropoff_datetime,
+    Affiliated_base_number,
+    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
+  from {{ source('staging','fhv_tripdata_external_table') }}
+   where dispatching_base_num is not null 
+)
+
+select 
+
+    -- identifiers
+    {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    cast(dispatching_base_num as string) as dispatching_base_num,
+    
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+        cast(Affiliated_base_number as string) as Affiliated_base_number                            
+    
+from tripdata
+
+where rn = 1
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
+
